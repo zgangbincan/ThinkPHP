5d20ebcbfa28120005926fa0f4bb628530b47c8e
nothing <zhangbc2012@gmail.com>
2015-03-12 15:23:23 +0800

    查看path_info 的环境

--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	var_dump($_SERVER['PATH_INFO']);
 		echo "hello world";
     }
     
