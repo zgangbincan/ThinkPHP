7c39695a670abc78e230081059ca1bc3f2a45fee
nothing <zhangbc2012@gmail.com>
2015-03-17 13:28:02 +0800

    打印环境变量

--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -3,8 +3,38 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	echo "path_info \n";
     	var_dump($_SERVER['PATH_INFO']);
+    	echo "server: \n";
     	print_r($_SERVER);
+    	echo "cookie \n";
+    	print_r($_COOKIE);
+		echo "env: \n";
+		print_r($_ENV);
+		echo "files: \n";
+		print_r($_FILES);
+		echo "get: \n";
+		print_r($_GET);
+		echo "post: \n";
+		print_r($_POST);
+		echo "request: \n";
+		print_r($_REQUEST);
+		echo "session: \n";
+		print_r($_SESSION);
+		echo "globals: \n";
+		print_r($GLOBALS);
+		echo "HTTP_ENV_VARS: \n";
+		print_r($HTTP_ENV_VARS);
+		echo "HTTP_GET_VARS: \n";
+		print_r($HTTP_GET_VARS);
+		echo "HTTP_POST_FILES";
+		print_r($HTTP_POST_FILES);
+		echo "HTTP_POST_VARS";
+		print_r($HTTP_POST_VARS);
+		echo "HTTP_SERVER_VARS";
+		print_r($HTTP_SERVER_VARS);
+		echo "HTTP_SESSION_VARS";
+		print_r($HTTP_SESSION_VARS);
 		echo "hello world";
     }
     
